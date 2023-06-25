c810a1d0b6c5c784a21da9c153513600e94fc6bc
jrobiche <33152499+jrobiche@users.noreply.github.com>
2023-06-24 21:08:25 -0500

    add content to about page

--- src/app/pages/about-page/about-page.component.ts
+++ src/app/pages/about-page/about-page.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { getTauriVersion } from '@tauri-apps/api/app';
+import { getVersion } from '@tauri-apps/api/app';
 
 @Component({
   selector: 'app-about-page',
   templateUrl: './about-page.component.html',
-  styleUrls: ['./about-page.component.sass']
+  styleUrls: ['./about-page.component.sass'],
 })
-export class AboutPageComponent {
+export class AboutPageComponent implements OnInit {
+  appVersion: string;
+  tauriVersion: string;
 
+  constructor() {
+    this.appVersion = 'unknown';
+    this.tauriVersion = 'unknown';
+  }
+
+  async ngOnInit(): Promise<void> {
+    this.appVersion = await getVersion();
+    this.tauriVersion = await getTauriVersion();
+  }
 }
